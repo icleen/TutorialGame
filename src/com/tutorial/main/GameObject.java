901259011cda398a8ccd2a7da282edbb2d903b8d
Iain <icleen@gmail.com>
2016-12-29 20:58:56 -0700

    My new tutorial game

---
+++ src/com/tutorial/main/GameObject.java
@@ -0,0 +1,51 @@
+package com.tutorial.main;
+
+import java.awt.Graphics;
+
+public abstract class GameObject {
+
+	protected int x, y;
+	protected ID id;
+	protected int velX, velY;
+	
+	public GameObject( int x, int y, ID id ) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+	
+	public abstract void tick();
+	public abstract void render( Graphics g );
+	
+	public void setX( int x ) {
+		this.x = x;
+	}
+	public int getX() {
+		return this.x;
+	}
+	public void setY( int y ) {
+		this.y = y;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setID( ID id ) {
+		this.id = id;
+	}
+	public ID getID() {
+		return this.id;
+	}
+	public void setJVelX( int x ) {
+		this.velX = x;
+	}
+	public int getVelX() {
+		return this.velX;
+	}
+	public void setJVelY( int y ) {
+		this.velY = y;
+	}
+	public int getVelY() {
+		return this.velY;
+	}
+	
+}
---
+++ src/com/tutorial/main/Handler.java
@@ -0,0 +1,16 @@
+package com.tutorial.main;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+public class Handler {
+		LinkedList<GameObject> object = new LinkedList<GameObject>();
+		
+		public void tick() {
+			
+		}
+		
+		public void render( Graphics g ) {
+			
+		}
+}
